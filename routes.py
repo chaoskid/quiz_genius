47e6d426b50035f979f0f1c577895d9484ca3a1b
Vaisakh <vaisakhpariyacheri@gmail.com>
2024-04-28 14:09:50 +1000

    Initial Commit2

--- routes.py
+++ routes.py
@@ -3,7 +3,6 @@ from flask_login import login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from authenticator import *
 from db import db, User, Quiz, Option, UserQuizScore, Question
-from question_form import QuizForm
 
 import bcrypt
 
